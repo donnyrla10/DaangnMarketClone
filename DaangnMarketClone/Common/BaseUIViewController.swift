37dd4a9d6ad24d40a8fe0df77cb8d5d7bf5a9faa
min <94814048+millizi@users.noreply.github.com>
2023-03-03 23:19:03 +0900

    ADD: 동네생활 탭 추가

---
+++ DaangnMarketClone/Common/BaseUIViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseUIViewController.swift
+//  DaangnMarketClone
+//
+//  Created by minimani on 2023/03/03.
+//
+
+import UIKit
+
+class BaseUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.modalPresentationStyle = .fullScreen
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ DaangnMarketClone/Main/VillageLife/View/VillageLifeTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  VillageLifeTableViewCell.swift
+//  DaangnMarketClone
+//
+//  Created by minimani on 2023/03/03.
+//
+
+import UIKit
+
+class VillageLifeTableViewCell: UITableViewCell {
+    init(reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
