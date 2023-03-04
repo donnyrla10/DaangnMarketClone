01edbe0e6088f9be2b4bdca6e09871100741fe2f
donnyrla10 <kokoo1214@naver.com>
2023-03-04 14:33:04 +0900

    ADD: 객체 상대값 기능 추가

--- DaangnMarketClone/Main/Home/PostingTableViewCell.swift
+++ DaangnMarketClone/Main/Home/PostingTableViewCell.swift
@@ -14,6 +14,7 @@ class PostingTableViewCell: UITableViewCell {
         title.numberOfLines = 2
         return title
     }()
+    
     let addressLabel = UILabel() //대학동
     let timeLabel = UILabel() //1시간 전
     let priceLabel = UILabel() //50,000원
@@ -21,13 +22,13 @@ class PostingTableViewCell: UITableViewCell {
     
     let chatButton = UIButton()
     let heartButton = UIButton()
+
     
     let postingImageView : UIImageView = {
         let imgView = UIImageView()
         imgView.layer.cornerRadius = 6
         imgView.contentMode = .scaleAspectFill
         imgView.clipsToBounds = true
-        imgView.layer.shadowRadius = 2
         return imgView
     }()
     
@@ -47,12 +48,33 @@ class PostingTableViewCell: UITableViewCell {
             $0.top.equalToSuperview().offset(12)
             $0.leading.equalToSuperview().offset(20)
             $0.bottom.equalToSuperview().offset(-12)
-            $0.width.height.equalTo(100)
+            $0.width.height.equalTo(100.fromIPX) //14 Pro 기준으로, 393 / 375 -> 1.048 * 100 = 104.8
         }
         self.contentView.addSubview(titleLabel)
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(12)
             $0.leading.equalTo(postingImageView.snp.trailing).offset(16)
         }
+    }
+}
+
+//객체 상대값 (width)
+extension CGFloat {
+    var fromIPX: CGFloat {
+        let screenIPXWidth = 375.0
+        let screenWidth = UIScreen.main.bounds.size.width
+        let ratio = screenWidth / screenIPXWidth
+        return self * ratio
+//        return CGFloat((Int((CGFloat(self) * ratio) * 2) + 1)) / 2.0        // 0.5 단위 round
+    }
+}
+
+extension Int {
+    var fromIPX: CGFloat {
+        let screenIPXWidth = 375.0
+        let screenWidth = UIScreen.main.bounds.size.width
+        let ratio = screenWidth / screenIPXWidth
+        return CGFloat(self) * ratio
+//        return CGFloat((Int((CGFloat(self) * ratio) * 2) + 1)) / 2.0        // 0.5 단위 round
     }
 }
